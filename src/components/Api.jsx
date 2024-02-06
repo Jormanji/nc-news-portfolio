78a176014a57cd6fd35c3967223fe9f112ff8c42
Jormanji <alacritydrummer@hotmail.co.uk>
2024-02-06 15:29:01 +0000

    Add vote function. Attempts at implementing some of the feedback

---
+++ src/components/Api.jsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseURL = 'https://nc-news-portfolio-site.onrender.com/api';
+
+const api = axios.create({
+  baseURL,
+});
+
+export default api;
--- src/components/IndividualArticles.jsx
+++ src/components/IndividualArticles.jsx
@@ -4,6 +4,7 @@ import axios from "axios"
 import { useParams } from "react-router-dom"
 import ArticleCardExpanded from "./ArticleCardExpanded"
 import Comments from "./Comments"
+import api from "./Api"
 
 
 export default function IndividualArticles (){
@@ -12,22 +13,27 @@ export default function IndividualArticles (){
     const [article, setArticle] = useState(null)
     const [comments, setComments] = useState([])
 
+
+
     useEffect(() => {
-        axios.get(`https://nc-news-portfolio-site.onrender.com/api/articles/${articleId}`)
-        .then((response) => {
-            setArticle(response.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-        axios.get(`https://nc-news-portfolio-site.onrender.com/api/articles/${articleId}/comments`)
-        .then((response) => {
-            setComments(response.data.comments)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }, [articleId])
+        api.get(`/articles/${articleId}`)
+          .then((response) => {
+            setArticle(response.data);
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+            setLoading(false);
+          });
+    
+        api.get(`/articles/${articleId}/comments`)
+          .then((response) => {
+            setComments(response.data.comments);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, [articleId]);
 
     if (!article) {
         return <div>Retrieving the article</div>;
