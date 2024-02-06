6967c1236947622330572d2a1695753b212e85c6
Jormanji <alacritydrummer@hotmail.co.uk>
2024-02-06 12:19:28 +0000

    Comment time change

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,7 +4,7 @@ export default function CommentCard ({comment}) {
     return(
         <div className="commentCard">
                 <p>Author: {comment.author}</p>
-                <p>Date Posted: {comment.created_at}</p>
+                <p>Date Posted: {comment.comment_created_at}</p>
                 <p>{comment.body}</p>
                 <p>Votes: {comment.votes}</p>
             </div>
